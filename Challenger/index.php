9c03dc17e7d645c580713babef827ce158453367
Albert Thompson <grantthompson123@hotmail.com>
2018-03-24 21:45:03 +1000

    Minor changes

--- Challenger/index.php
+++ Challenger/index.php
@@ -49,17 +49,13 @@
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title">Modal title</h5>
+        <h5 class="modal-title" id="header"></h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
       <div class="modal-body" id="body">
         
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
       </div>
     </div>
   </div>
@@ -71,9 +67,11 @@
   
   $(document).ready( function() {
     $("#login").on("click", function() {
+        $("#header").append("Login");
         $("#body").load("/Challenger/login.php");
     });
     $("#register").on("click", function() {
+        $("#header").append("Register");
         $("#body").load("/Challenger/register.php");
     });
 });
--- Challenger/register.php
+++ Challenger/register.php
@@ -1,14 +1,17 @@
 <html>
   <head>
-    <title>My first PHP website</title>
   </head>
   <body>
-    <h2>Registration Page</h2>
-    <a href="index.php">Click here to go back</a><br/><br/>
+    <script src=https://code.jquery.com/jquery-3.2.1.min.js></script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+       
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
     <form action="register.php" method="post">
       Enter Username: <input type="text" name="username" required="required"/> <br/>
       Enter Password: <input type="password" name="password" required="required" /> <br/>
-      <input type="submit" value="Register"/>
+      <input type="submit" class="btn btn-primary" value="Register"/>
     </form>
   </body>
 </html>
