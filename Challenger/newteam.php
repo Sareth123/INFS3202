89fbec4c96e80c9c030ec6795e93d2b4f7873c55
Albert Thompson <grantthompson123@hotmail.com>
2018-04-12 18:12:54 +1000

    Add a feature for adding more emails, next step would having this feature send out emails

--- Challenger/newteam.php
+++ Challenger/newteam.php
@@ -8,9 +8,42 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
         
-    <form action="checknewteam.php" method="post">
+ <form action="checknewteam.php" method="post">
+   
       <p>Enter Team Name: <input type="text" name="name" required="required"/></p> <br/>
-      <input type="submit" class="btn btn-primary" value="Create Team"/>
+
+  <div class="email_field">
+      <p>Please enter in 5 of team memebers email addresses:</p>
+        <input type="text" name="email1" required="required"/><br>
+        <input type="text" name="email2" required="required"/><br>
+        <input type="text" name="email3" required="required"/><br>
+        <input type="text" name="email4" required="required"/><br>
+        <input type="text" name="email5" required="required"/><br>
+        <p class="btn btn-primary" id="add" title="Add field">Add</p>
+    </div>
+    <input type="submit" class="btn btn-primary" value="Create Team"/>
     </form>
     </body>
 </html>
+
+<script type="text/javascript">
+$(document).ready(function(){
+    var count = 1;
+    var max = 7; //Allow for a total of 7 email fields be visible
+    var add = $('#add');
+    var email_field = $('.email_field'); 
+    var newEmail = '<div><input type="text" name="email" value=""/><p class="btn btn-secondary" id="remove" title="Remove field">Remove</p></div>'; //new email text field
+    
+    $(add).click(function(){ 
+        if(count < max){
+            count++;
+            $(email_field).append(newEmail);
+        }
+    });
+    $(email_field).on('click', '#remove', function(e){
+        e.preventDefault();
+        $(this).parent('div').remove();
+        count--;
+    });
+});
+</script>
