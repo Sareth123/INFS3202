341465b750cb9cd9dffa9a54a97d8dc0f8f8046f
Albert Thompson <grantthompson123@hotmail.com>
2018-04-22 15:56:11 +1000

    removing stuff

--- Challenger/Team.php
+++ Challenger/Team.php
@@ -41,40 +41,5 @@
         </nav>
     </head>
     <body>
-        <div align="center">
-            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#content" id="login">Login</button>
-            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#content" id="register">Register</button>
-        </div>
     </body>
-      <div class="modal fade" id="content" tabindex="-1" role="dialog">
-        <div class="modal-dialog" role="document">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title" id="header"></h5>
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-              </button>
-            </div>
-            <div class="modal-body" id="body">
-              
-            </div>
-          </div>
-        </div>
-      </div>
-      <script>
-          $('#myModal').on('shown.bs.modal', function () {
-        $('#myInput').trigger('focus')
-      });
-        
-        $(document).ready( function() {
-          $("#login").on("click", function() {
-              $("#header").append("Login");
-              $("#body").load("/Challenger/login.php");
-          });
-          $("#register").on("click", function() {
-              $("#header").append("Register");
-              $("#body").load("/Challenger/register.php");
-          });
-      });
-
-      </script>
+  
