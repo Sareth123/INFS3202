01f6effd35d9d3a414647f4661c2e05733c4ad61
Ciaran Lawson <ciaran567@gmail.com>
2018-04-23 12:14:15 +1000

    Adding navbar to each page and fixing up some syntax

--- Challenger/ContactUs.php
+++ Challenger/ContactUs.php
@@ -2,43 +2,46 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <script src=https://code.jquery.com/jquery-3.2.1.min.js></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
         <title>Challenger</title>
-        <nav class="navbar navbar-default">
-          <div class="container">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-              <a class="navbar-brand" href="#">Challenger</a>
-            </div>
-
-            <!-- Collect the nav links, forms, and other content for toggling -->
-              <ul class="nav navbar-nav">
-                <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
-                <li><a href="#">Link</a></li>
-                <li class="dropdown">
-                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-                  <ul class="dropdown-menu">
-                    <li><a href="#">Action</a></li>
-                    <li><a href="#">Another action</a></li>
-                    <li><a href="#">Something else here</a></li>
-                    <li role="separator" class="divider"></li>
-                    <li><a href="#">Separated link</a></li>
-                    <li role="separator" class="divider"></li>
-                    <li><a href="#">One more separated link</a></li>
-                  </ul>
-                </li>
-              </ul>
-              <form class="navbar-form navbar-left">
-                <div class="form-group">
-                  <input type="text" class="form-control" placeholder="Search">
-                </div>
-                <button type="submit" class="btn btn-default">Submit</button>
-              </form>
-          </div><!-- /.container-fluid -->
-        </nav>
     </head>
+    
     <body>
+      <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+        <div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="/INFS3202/Challenger/Ladder.php">Ladder</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/INFS3202/Challenger/Rules.php">Rules</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/INFS3202/Challenger/AboutUs.php">About Us</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/INFS3202/Challenger/ContactUs.php">Contact Us</a>
+                </li>
+                </ul>
+        </div>
+          <div class="mx-auto order-0">
+            <a class="navbar-brand mx-auto" href="#">Challenger</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".dual-collapse2">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+          </div>
+        <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
+          <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                  <a class="nav-link" href="/INFS3202/Challenger/Profile.php">Profile</a>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link" href="/INFS3202/Challenger/Team.php">Team</a>
+              </li>
+          </ul>
+        </div>
+      </nav>
     </body>
