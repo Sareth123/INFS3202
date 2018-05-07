a3dd8f6aaf26e0f2e476f8b6f50a094031ce0e37
Albert Thompson <grantthompson123@hotmail.com>
2018-05-07 16:29:25 +1000

    basic submit of a challenge

--- Challenger/matches.php
+++ Challenger/matches.php
@@ -1,13 +1,12 @@
 <?php 
-
 $query= mysqli_query($db->link,"SELECT t1.name, MAX(c.date) AS d, CASE
 					WHEN c.team1_id!=t1.team_id THEN c.t1_score-c.t2_score
    					WHEN c.team1_id=t1.team_id THEN c.t2_score-c.t1_score
     			END AS result
 		FROM teams AS t, teams AS t1,  challenges AS c
-		WHERE t.name=('$team_name') AND
+		WHERE t.name=('$other_team') AND
 		IF(t.team_id= c.team2_id,  c.team1_id =t1.team_id ,c.team2_id=t1.team_id)");
-$query1=mysqli_query($db->link,"SELECT wins, losses FROM teams WHERE name=('$team_name')");
+$query1=mysqli_query($db->link,"SELECT wins, losses FROM teams WHERE name=('$other_team')");
 
 	
 ?>
